ce5feb5de95188e320cf4bb840d4d54c2eae39b1
fvn70 <fvn_reg@mail.ru>
2022-02-08 15:20:32 +0700

    new Stage 4

--- Honest Calculator/task/honest_calc.py
+++ Honest Calculator/task/honest_calc.py
@@ -1,9 +1,30 @@
+def is_one_digit(v):
+    v1 = v * 10
+    return v1 < 100 and v1 % 10 == 0
+
+
+def check(v1, v2, v3):
+    msg = ""
+    if is_one_digit(v1) and is_one_digit(v2):
+        msg += msg_6
+    if (v1 == 1 or v2 == 1) and v3 == "*":
+        msg += msg_7
+    if (v1 == 0 or v2 == 0) and v3 in "*-+":
+        msg += msg_8
+    if msg != "":
+        print(msg_9 + msg)
+
+
 msg_0 = "Enter an equation"
 msg_1 = "Do you even know what numbers are? Stay focused!"
 msg_2 = "Yes ... an interesting math operation. You've slept through all classes, haven't you?"
 msg_3 = "Yeah... division by zero. Smart move..."
 msg_4 = "Do you want to store the result? (y / n):"
 msg_5 = "Do you want to continue calculations? (y / n):"
+msg_6 = " ... lazy"
+msg_7 = " ... very lazy"
+msg_8 = " ... very, very lazy"
+msg_9 = "You are"
 
 memory = 0.0
 exit_main = False
@@ -24,6 +45,8 @@ while not exit_main:
     if not oper in "+-*/":
         print(msg_2)
         continue
+
+    check(x, y, oper)
 
     if oper == '+':
         result = x + y
